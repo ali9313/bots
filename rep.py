1ceb45aa94f43f691dd60415bed1af12973dd03c
ali9313 <lioali65808@gmail.com>
2024-09-23 01:30:33 +0300

    Add files via upload

--- rep.py
+++ rep.py
@@ -1,32 +1,31 @@
 from config import *
 from insert_replyy import *
 from read_replyy import *
+
 def reply_func(a):
     if a.text == "اهلا":
         bot.reply_to(a, "مرحبا")
     elif a.text == "باي":
         bot.reply_to(a, "الله ياخذك")
     elif a.text == "ملاك":
-        bot.reply_to(a, "͏
-
+        bot.reply_to(a, """͏
                             ╱|、
                           (˚ˎ 。7  
                            |、˜〵          
-                          じしˍ,)ノ")
+                          じしˍ,)ノ""")
     elif "تمساح" in a.text:
         bot.reply_to(a, "يبحث عن ثلاثينية")
     elif a.text == "بوت":
         bot.reply_to(a, "ها شتريد")
     elif a.text == "تمسوح":
-    	bot.send_photo(a.chat.id,open("pic/tmsah.jpg","rb"))
+        bot.send_photo(a.chat.id, open("pic/tmsah.jpg", "rb"))
     elif a.text == "موسيقى":
-    	bot.send_voice(a.chat.id,open("voice/vv.m4a","rb"))
+        bot.send_voice(a.chat.id, open("voice/vv.m4a", "rb"))
     elif a.text == "طرد" or a.text == "حظر":
-            bnn = bot.ban_chat_member(a.chat.id, a.reply_to_message.from_user.id)
-            if bnn:       
-                    bot.send_message(a.chat.id, "تم دفر: @" + a.reply_to_message.from_user.username)
+        bnn = bot.ban_chat_member(a.chat.id, a.reply_to_message.from_user.id)
+        if bnn:       
+            bot.send_message(a.chat.id, "تم دفر: @" + a.reply_to_message.from_user.username)
     elif "اضف" in a.text:
-                insert_rep(a)
+        insert_rep(a)
     else:
-                my_txtrep(a)
-                
+        my_txtrep(a)
