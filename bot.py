9effdfbfa38869d652c53e43996686d8054ef29f
ali9313 <lioali65808@gmail.com>
2024-09-22 14:33:51 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -14,6 +14,8 @@ def echo_message(a):
         bot.reply_to(a, "الله ياخذك")
     elif a.text == "ملاك":
         bot.reply_to(a, "القطة المشاكسه")
+    elif a.text =="تمساح":
+    	bot.reply_to(a,"يبحث عن ثلاثينية")
     else:
         bot.reply_to(a, "لم افهمك ابدا")
 
