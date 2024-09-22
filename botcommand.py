8236b73575887de208e82de5cf1b157399027a10
ali9313 <lioali65808@gmail.com>
2024-09-22 17:57:13 +0300

    Add files via upload

--- botcommand.py
+++ botcommand.py
@@ -3,4 +3,4 @@ def my_cmd(a):
 	if a.text=="/ban":
 		bnn=bot.ban_chat_member(a.chat.id,a.reply_to_message.from_user.id)
 		if bnn:
-			bot.send_message(a.chat.id,"تم دفر:"+"  @"+ a.reply_to_message.from_username)
+			bot.send_message(a.chat.id, "تم دفر: @" + a.reply_to_message.from_user.username)
