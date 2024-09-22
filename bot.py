71e5fc526a622be3730edcbebb350c8885621478
ali9313 <lioali65808@gmail.com>
2024-09-22 14:08:44 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -10,6 +10,8 @@ def echo_message(a):
     if a.text=="اهلا":
           bot.reply_to(a, "مرحبا")
           elif a.text=="باي":
+          	elif a.text=="ملاك":
+          		bot.reply_to(a,"القطة المشاكسه")
           bot.reply_to(a,"الله ياخذك")
     else:
     	bot.reply_to(a,"لم افهمك ابدا")
