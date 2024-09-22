49e1dddef03d8244d7d5af25fb7b8aeaed4e2974
ali9313 <lioali65808@gmail.com>
2024-09-23 01:13:40 +0300

    Add files via upload

--- insert_replyy.py
+++ insert_replyy.py
@@ -4,6 +4,6 @@ def insert_rep(a):
 	tmsg=tmsg.split()[1:]
 	tmsg=" ".join(map(str , tmsg))
 	z=open("backend/m_replyy.txt","a",encoding="utf-8")
-	z.write("/n")
+	z.write("\n")
 	z.write(tmsg)
 	z.close()
