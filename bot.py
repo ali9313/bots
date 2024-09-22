11b363eaef2f71989b104fc64be9de0397a2ba02
ali9313 <lioali65808@gmail.com>
2024-09-22 14:23:57 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -5,15 +5,16 @@ bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, "ترسل اهلا ارد عليك مرحبا ، ترسل غير شي ما افهم ترا")
+
 @bot.message_handler(func=lambda a: True)
 def echo_message(a):
-    if a.text=="اهلا":
-          bot.reply_to(a, "مرحبا")
-          elif a.text=="باي":
-          bot.reply_to(a,"الله ياخذك")
-          	elif a.text=="ملاك":
-          		bot.reply_to(a,"القطة المشاكسه")
+    if a.text == "اهلا":
+        bot.reply_to(a, "مرحبا")
+    elif a.text == "باي":
+        bot.reply_to(a, "الله ياخذك")
+    elif a.text == "ملاك":
+        bot.reply_to(a, "القطة المشاكسه")
     else:
-    	bot.reply_to(a,"لم افهمك ابدا")
-    	
+        bot.reply_to(a, "لم افهمك ابدا")
+
 bot.polling()
