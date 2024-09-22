4c71fd1f106a418056d66b98a15229d46b78ae6b
ali9313 <lioali65808@gmail.com>
2024-09-22 15:09:54 +0300

    Add files via upload

---
+++ rep.py
@@ -0,0 +1,13 @@
+from config import *
+bot = telebot.TeleBot(TOKEN)
+def reply_func(a):
+	if a.text == "اهلا":
+        bot.reply_to(a, "مرحبا")
+    elif a.text == "باي":
+        bot.reply_to(a, "الله ياخذك")
+    elif a.text=="ملاك"
+        bot.reply_to(a,"القطة")
+    elif a.text =="تمساح":
+    	bot.reply_to(a,"يبحث عن ثلاثينية")
+    else:
+        bot.reply_to(a, "لم افهمك ابدا")
