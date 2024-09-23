c6ad0218e250c15a5f2c11fb499f421014663d1e
ali9313 <lioali65808@gmail.com>
2024-09-23 16:05:13 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -3,5 +3,6 @@ import telebot
 import time
 from telebot.types import Message
 from telebot.types import InlineKeyboardMarkup
+import telegram
 TOKEN = os.environ.get("TOKEN")
 bot = telebot.TeleBot(TOKEN)
