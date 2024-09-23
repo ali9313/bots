c589feb14003a07c134f7528fea9fe15208bef80
ali9313 <lioali65808@gmail.com>
2024-09-23 16:00:00 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ def cmbr(a):
 	bot.delete_message(a.chat.id,a.message_id)
 @bot.message_handler(commands=['start','ban'])
 def my(a):
-	my_cmd(a: Update, context: CallbackContext)
+	my_cmd(a)
 def send_welcome(message):
     bot.reply_to(message, "ترسل اهلا ارد عليك مرحبا ، ترسل غير شي ما افهم ترا")
 @bot.message_handler(func=lambda a: True)
--- botcommand.py
+++ botcommand.py
@@ -1,9 +1,13 @@
 from config import * 
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
+
+# دالة التحقق من إذا كان المستخدم مشرفًا
 def is_user_admin(bot, chat_id, user_id):
     user_status = bot.get_chat_member(chat_id, user_id).status
-    return user_status in ['administrator', 'creator'] 
+    return user_status in ['administrator', 'creator']
+
+# دالة التعامل مع أمر الحظر
 def my_cmd(a: Update, context: CallbackContext):
     message = a.message
     user = message.from_user
@@ -13,8 +17,14 @@ def my_cmd(a: Update, context: CallbackContext):
         if is_user_admin(context.bot, chat_id, user.id):
             try:
                 banned_user_id = message.reply_to_message.from_user.id
-                context.bot.ban_chat_member(chat_id, banned_user_id)
-                context.bot.send_message(chat_id, f"تم دفر: @{message.reply_to_message.from_user.username}")
+                banned_user_status = context.bot.get_chat_member(chat_id, banned_user_id).status
+                
+                # التحقق من أن المستخدم ليس مشرفًا أو منشئ المجموعة
+                if banned_user_status not in ['administrator', 'creator']:
+                    context.bot.ban_chat_member(chat_id, banned_user_id)
+                    context.bot.send_message(chat_id, f"تم دفر: @{message.reply_to_message.from_user.username}")
+                else:
+                    context.bot.send_message(chat_id, "لا يمكن حظر مشرف أو منشئ المجموعة.")
             except Exception as e:
                 context.bot.send_message(chat_id, f"حدث خطأ أثناء محاولة الحظر: {str(e)}")
         else:
