253b0065964e20a5b89fc559b90063bc8dfd7b2f
ali9313 <lioali65808@gmail.com>
2024-09-23 23:32:07 +0300

    Add files via upload

--- botcommand.py
+++ botcommand.py
@@ -8,7 +8,6 @@ def my_cmd(a):
             user_to_ban = a.reply_to_message.from_user.id
             chat_id = a.chat.id
 
-
             user_to_ban_member = bot.get_chat_member(chat_id, user_to_ban)
             if user_to_ban_member.status not in ['administrator', 'creator']:
                 try:
@@ -16,11 +15,10 @@ def my_cmd(a):
                     bot.reply_to(a, f"تم دفر: @{a.reply_to_message.from_user.username if a.reply_to_message.from_user.username else 'Unknown'}")
                 except Exception as e:
                     bot.reply_to(a, f"حدث خطأ: {str(e)}")
-              elif user_to_ban_member.status in ['administrator', 'creator']:
-            try:
+            elif user_to_ban_member.status in ['administrator', 'creator']:
                 bot.reply_to(a, "مشرف هذا حبي")
-                else:
-                	bot.reply_to(a,"يا مطي تريد تحظر نفسك !")
+            else:
+                bot.reply_to(a, "يا مطي تريد تحظر نفسك !")
         else:
             bot.reply_to(a, "استخدم الامر بالرد ")
     else:
