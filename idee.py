695ab53fa79ae387370ab464949b6006c7dea195
ali9313 <lioali65808@gmail.com>
2024-09-23 22:12:57 +0300

    Add files via upload

--- idee.py
+++ idee.py
@@ -1,13 +1,11 @@
 from config import bot
 
 def send_user_info(a):
-    
     user = a.from_user
     user_name = user.first_name
     user_username = user.username if user.username else "معنده"
     user_messages_count = a.message_id
 
-   
     message_text = f"""
     ⋆─┄─┄─┄─┄─⋆
     ‣ NAME ⇢ {user_name}
@@ -16,11 +14,9 @@ def send_user_info(a):
     ⋆─┄─┄─┄─┄─⋆
     """
 
- 
     photos = bot.get_user_profile_photos(user.id)
 
     if photos.total_count > 0:
-        return
         bot.send_photo(a.chat.id, photos.photos[0][-1].file_id, caption=message_text)
-    else:
-        bot.send_message(a.chat.id, message_text)
+    else:
+        bot.send_message(a.chat.id, message_text)
