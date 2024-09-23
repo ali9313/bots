efc802fd10b393c0cd38fa6fa1affdb54103f432
ali9313 <lioali65808@gmail.com>
2024-09-23 17:45:02 +0300

    Add files via upload

--- botcommand.py
+++ botcommand.py
@@ -7,24 +7,22 @@ def is_user_admin(chat_id, user_id):
         return False  
 def my_cmd(a):
     user = a.from_user
-    chat_id = a.chat.id  # استخدام chat.id بدلاً من chat_id
+    chat_id = a.chat.id 
 
     if a.reply_to_message:
         if is_user_admin(chat_id, user.id):
             try:
                 banned_user_id = a.reply_to_message.from_user.id
                 banned_user_status = bot.get_chat_member(chat_id, banned_user_id).status
-                
-                # التحقق من أن المستخدم ليس مشرفًا أو منشئ المجموعة
                 if banned_user_status not in ['administrator', 'creator']:
                     bot.ban_chat_member(chat_id, banned_user_id)
                     username = a.reply_to_message.from_user.username or "المستخدم"
-                    bot.send_message(chat_id, f"تم حظر: @{username}")
+                    bot.send_message(chat_id, f"تم دفر: @{username}")
                 else:
-                    bot.send_message(chat_id, "لا يمكن حظر مشرف أو منشئ المجموعة.")
+                    bot.send_message(chat_id, "ما اكدر احظر مشرف اعذرني ")
             except Exception as e:
-                bot.send_message(chat_id, f"حدث خطأ أثناء محاولة الحظر: {str(e)}")
+                bot.send_message(chat_id, f"اكو شي غلط: {str(e)}")
         else:
-            bot.send_message(chat_id, "عذراً، يجب أن تكون مشرفًا لاستخدام هذا الأمر.")
+            bot.send_message(chat_id, "انت مو مشرف يا مطي ")
     else:
-        bot.send_message(chat_id, "يرجى الرد على رسالة المستخدم الذي تريد حظره.")
+        bot.send_message(chat_id, "استخدم الامر بالرد ")
