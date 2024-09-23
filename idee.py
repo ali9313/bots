7a7340dd05ad65aa8446efe7b6b59912c0206d8e
ali9313 <lioali65808@gmail.com>
2024-09-23 22:07:42 +0300

    Add files via upload

--- idee.py
+++ idee.py
@@ -1,13 +1,13 @@
 from config import bot
 
 def send_user_info(a):
-    # جلب معلومات المستخدم
+    
     user = a.from_user
     user_name = user.first_name
     user_username = user.username if user.username else "معنده"
     user_messages_count = a.message_id
 
-    # إنشاء الكليشة
+   
     message_text = f"""
     ⋆─┄─┄─┄─┄─⋆
     ‣ NAME ⇢ {user_name}
@@ -16,16 +16,11 @@ def send_user_info(a):
     ⋆─┄─┄─┄─┄─⋆
     """
 
-    # جلب صورة البروفايل الخاصة بالمستخدم
+ 
     photos = bot.get_user_profile_photos(user.id)
 
     if photos.total_count > 0:
-        # إذا كانت هناك صورة، إرسال الصورة أولاً
-        bot.send_photo(a.chat.id, photos.photos[0][-1].file_id)
-    
-    # إرسال الكليشة
-    bot.send_message(a.chat.id, message_text)
-
-    # إذا لم تكن هناك صورة، يمكن إرسال رسالة إضافية (اختياري)
-    if photos.total_count == 0:
-         bot.send_message(a.chat.id, message_text)
+        return
+        bot.send_photo(a.chat.id, photos.photos[0][-1].file_id, caption=message_text)
+    else:
+        bot.send_message(a.chat.id, message_text)
