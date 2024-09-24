5c6eb5f09ba6e439e0cbb3062120e848271e1f5e
ali9313 <lioali65808@gmail.com>
2024-09-24 15:52:41 +0300

    Add files via upload

--- rtb.py
+++ rtb.py
@@ -1,21 +1,23 @@
 from config import *
+from collections import defaultdict
+
 roles = {
     'عضو': 1,
     'مدير': 2,
     'مهيب': 3
 }
-MAHIIB_ID = 232499688 
+MAHIIB_ID = 232499688
 
 # قائمة الأعضاء مع رتبهم
 members = defaultdict(lambda: 'عضو')  # افتراضي: عضو
 
-# دالة لمنح رتبة لأحد الأعضاء
+# دالة لمنح رتبة لأحد الأعضاء من خلال الرد على رسالته
 def promote_user(a):
     user_id = a.from_user.id
     if user_id == MAHIIB_ID:  # فقط المهيب يمكنه منح الرتب
-        if len(a.text.split()) > 1:
-            target_user_id = int(a.text.split()[1])  # الحصول على ID المستخدم المستهدف
-            new_role = a.text.split()[2] if len(a.text.split()) > 2 else 'عضو'  # الحصول على الرتبة الجديدة أو افتراضيًا 'عضو'
+        if a.reply_to_message:
+            target_user_id = a.reply_to_message.from_user.id  # الحصول على ID المستخدم المستهدف من الرسالة المُرد عليها
+            new_role = a.text.split()[1] if len(a.text.split()) > 1 else 'عضو'  # الحصول على الرتبة الجديدة أو افتراضيًا 'عضو'
             
             if new_role in roles:
                 members[target_user_id] = new_role
@@ -23,11 +25,11 @@ def promote_user(a):
             else:
                 bot.reply_to(a, "رتبة غير صحيحة. يمكن أن تكون الرتبة: عضو، مدير، أو مهيب.")
         else:
-            bot.reply_to(a, "يرجى تقديم ID المستخدم والرتبة التي تريد ترقيته لها.")
+            bot.reply_to(a, "يرجى الرد على رسالة المستخدم الذي تريد ترقيته.")
     else:
         bot.reply_to(a, "فقط المهيب يمكنه منح الرتب.")
 
-# دالة لقراءة رتبة مستخدم
+# دالة لقراءة رتبة مستخدم من خلال الرد على رسالته
 def read_role(a):
     user_id = a.from_user.id
     if a.reply_to_message:
