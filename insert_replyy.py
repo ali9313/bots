266f3fc939cb8f544d7550dcd703fc9a887af576
ali9313 <lioali65808@gmail.com>
2024-09-24 23:37:56 +0300

    Delete insert_replyy.py

--- insert_replyy.py
+++
@@ -1,9 +0,0 @@
-from config import *
-def insert_rep(a):
-	tmsg=a.text
-	tmsg=tmsg.split()[1:]
-	tmsg=" ".join(map(str , tmsg))
-	z=open("backend/m_replyy.txt","a",encoding="utf-8")
-	z.write("\n")
-	z.write(tmsg)
-	z.close()
