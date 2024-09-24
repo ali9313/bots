14cfca3788f49dcdf4fe5878a46f6fe3dce48c20
ali9313 <lioali65808@gmail.com>
2024-09-24 11:56:52 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ from config import *
 from rep import *
 from botcommand import *
 from mut import *
+
 @bot.message_handler(content_types=['new_chat_members', 'left_chat_members'])
 def cmbr(a):
     bot.delete_message(a.chat.id, a.message_id)
@@ -16,13 +17,17 @@ def my(a):
 def send_welcome(a):
     bot.reply_to(a, "ترسل اهلا ارد عليك مرحبا ، ترسل غير شي ما افهم ترا")
 
-@bot.message_handler(func=lambda a: True)
-def echo_message(a):
-    reply_func(a) 
+# معالج حذف الرسائل للمستخدمين المكتمين
 @bot.message_handler(func=lambda a: a.from_user.id in muted_users)
 def delete_muted_message(a):
     try:
         bot.delete_message(a.chat.id, a.message_id)
     except Exception as e:
         bot.reply_to(a, f"حدث خطأ عند محاولة حذف الرسالة: {str(e)}")
+
+# معالج الرسائل العامة (يجب أن يأتي بعد معالج حذف الرسائل)
+@bot.message_handler(func=lambda a: True)
+def echo_message(a):
+    reply_func(a) 
+
 bot.polling()
