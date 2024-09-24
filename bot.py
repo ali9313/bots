d5dcabd6ccf480678e0e3f0d63694f072da3f9ee
ali9313 <lioali65808@gmail.com>
2024-09-25 01:52:15 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -4,28 +4,22 @@ from botcommand import *
 from mut import *
 from rdod import load_responses  # استيراد دالة تحميل الردود
 
-# متغير لتحديد حالة البوت
-is_running = False
-
 @bot.message_handler(content_types=['new_chat_members', 'left_chat_members'])
 def cmbr(a):
-    if is_running:
-        bot.delete_message(a.chat.id, a.message_id)
+    bot.delete_message(a.chat.id, a.message_id)
 
 @bot.message_handler(commands=['start', 'ban'])
 def my(a):
-    if is_running:
-        if a.text == "/ban" and a.reply_to_message:
-            my_cmd(a)
-        else:
-            send_welcome(a)
+    if a.text == "/ban" and a.reply_to_message:
+        my_cmd(a)
+    else:
+        send_welcome(a)
 
 def send_welcome(a):
-    if is_running:
-        bot.reply_to(a, "ترسل اهلا ارد عليك مرحبا ، ترسل غير شي ما افهم ترا")
+    bot.reply_to(a, "ترسل اهلا ارد عليك مرحبا ، ترسل غير شي ما افهم ترا")
 
 # معالج حذف الرسائل للمستخدمين المكتمين
-@bot.message_handler(func=lambda a: is_running and a.from_user.id in muted_users)
+@bot.message_handler(func=lambda a: a.from_user.id in muted_users)
 def delete_muted_message(a):
     try:
         bot.delete_message(a.chat.id, a.message_id)
@@ -33,10 +27,9 @@ def delete_muted_message(a):
         bot.reply_to(a, f"حدث خطأ عند محاولة حذف الرسالة: {str(e)}")
 
 # معالج الرسائل العامة (يجب أن يأتي بعد معالج حذف الرسائل)
-@bot.message_handler(func=lambda a: is_running)
+@bot.message_handler(func=lambda a: True)
 def echo_message(a):
     reply_func(a)
 
-# بدء تشغيل البوت وتفعيل المتغير
-is_running = True
+# بدء تشغيل البوت
 bot.polling()
