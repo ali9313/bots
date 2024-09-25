9c7205000e342ba442290f3b483da70d536eb827
ali9313 <lioali65808@gmail.com>
2024-09-25 11:36:05 +0300

    Add files via upload

--- rep.py
+++ rep.py
@@ -1,9 +1,4 @@
 from config import *
-from idee import *
-from mut import *
-from rtb import *
-from rdod import *
-
 def reply_func(a):
     if a.text == "اهلا":
         bot.reply_to(a, "مرحبا")
@@ -27,23 +22,5 @@ def reply_func(a):
     elif a.text == "موسيقى":
         with open("voice/vv.m4a", "rb") as audio:
             bot.send_audio(a.chat.id, audio)
-    elif a.text in ["طرد", "حظر"]:
-        my_cmd(a)
-    elif a.text in ["كتم", "لصم"]:
-        mute_user(a)
-    elif a.text == "الغاء كتم":
-        unmute_user(a)
-    elif a.text in ["ايدي", "ا"]:
-        send_user_info(a)
-    elif "رفع" in a.text:
-        promote_user(a)
-    elif a.text == "رتبته":
-        read_role(a)
-    elif a.text == "اضف رد":
-        start_adding_response(a)
-    elif a.text == "حذف رد":
-        start_deleting_response(a)
-    elif a.text == "الردود":
-        show_responses(a)
     else:
         bot.reply_to(a, "طيطي")
