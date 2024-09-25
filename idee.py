01717b2c8571fff92f7e295e4898d52335e722af
ali9313 <lioali65808@gmail.com>
2024-09-25 16:43:25 +0300

    Add files via upload

--- idee.py
+++ idee.py
@@ -1,6 +1,6 @@
 # هذا الكود في الملف الثاني
 from config import bot
-from rtb import load_roles  # استدعاء دالة قراءة الرتب من الملف الأول
+from rtb import *  # استدعاء دالة قراءة الرتب من الملف الأول
 
 # قاموس لتخزين عدد الرسائل لكل مستخدم
 user_message_count = {}
