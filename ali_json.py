b8b04316e2d5eb798ee1ca6df3faf8508710c2c9
ali9313 <lioali65808@gmail.com>
2024-09-29 02:06:21 +0300

    Add files via upload

--- ali_json.py
+++ ali_json.py
@@ -1,166 +1,156 @@
 from config import *
-import json
 
 DEVELOPERS = [232499688]
 OWNER_BOT = 232499688
 
-# دوال تحميل وتفريغ الملفات
+# دوال تحميل وتفريغ الملفات النصية
 def load_ali_basic_devs():
+    ali_basic_devs = {}
     try:
-        with open('backend/ali_basic_devs.json', 'r') as file:
-            ali_basic_devs = json.load(file)
+        with open('backend/ali_basic_devs.txt', 'r') as file:
+            for line in file:
+                dev_id = line.strip()
+                ali_basic_devs[dev_id] = True
     except FileNotFoundError:
-        print("الملف 'ali_basic_devs.json' غير موجود.")
-        ali_basic_devs = {'basic_devs': {}}
-    except json.JSONDecodeError:
-        print("خطأ في قراءة بيانات JSON من 'ali_basic_devs.json'.")
-        ali_basic_devs = {'basic_devs': {}}
-    except Exception as e:
-        print(f"حدث خطأ غير متوقع أثناء تحميل 'ali_basic_devs.json': {e}")
-        ali_basic_devs = {'basic_devs': {}}
+        print("الملف 'ali_basic_devs.txt' غير موجود.")
     return ali_basic_devs
 
 def dump_ali_basic_devs(ali_basic_devs):
     try:
-        with open('backend/ali_basic_devs.json', 'w') as file:
-            json.dump(ali_basic_devs, file)
+        with open('backend/ali_basic_devs.txt', 'w') as file:
+            for dev_id in ali_basic_devs:
+                file.write(f"{dev_id}\n")
     except Exception as e:
-        print(f"حدث خطأ أثناء تفريغ 'ali_basic_devs.json': {e}")
+        print(f"حدث خطأ أثناء تفريغ 'ali_basic_devs.txt': {e}")
 
 def load_ali_devs():
+    ali_devs = {}
     try:
-        with open('backend/ali_devs.json', 'r') as file:
-            ali_devs = json.load(file)
+        with open('backend/ali_devs.txt', 'r') as file:
+            for line in file:
+                dev_id = line.strip()
+                ali_devs[dev_id] = True
     except FileNotFoundError:
-        print("الملف 'ali_devs.json' غير موجود.")
-        ali_devs = {'devs': {}}
-    except json.JSONDecodeError:
-        print("خطأ في قراءة بيانات JSON من 'ali_devs.json'.")
-        ali_devs = {'devs': {}}
-    except Exception as e:
-        print(f"حدث خطأ غير متوقع أثناء تحميل 'ali_devs.json': {e}")
-        ali_devs = {'devs': {}}
+        print("الملف 'ali_devs.txt' غير موجود.")
     return ali_devs
 
 def dump_ali_devs(ali_devs):
     try:
-        with open('backend/ali_devs.json', 'w') as file:
-            json.dump(ali_devs, file)
+        with open('backend/ali_devs.txt', 'w') as file:
+            for dev_id in ali_devs:
+                file.write(f"{dev_id}\n")
     except Exception as e:
-        print(f"حدث خطأ أثناء تفريغ 'ali_devs.json': {e}")
+        print(f"حدث خطأ أثناء تفريغ 'ali_devs.txt': {e}")
 
 def load_ali_basic_creators():
+    ali_basic_creators = {}
     try:
-        with open('backend/ali_basic_creators.json', 'r') as file:
-            ali_basic_creators = json.load(file)
+        with open('backend/ali_basic_creators.txt', 'r') as file:
+            for line in file:
+                creator_id = line.strip()
+                ali_basic_creators[creator_id] = True
     except FileNotFoundError:
-        print("الملف 'ali_basic_creators.json' غير موجود.")
-        ali_basic_creators = {'basic_creators': {}}
-    except json.JSONDecodeError:
-        print("خطأ في قراءة بيانات JSON من 'ali_basic_creators.json'.")
-        ali_basic_creators = {'basic_creators': {}}
-    except Exception as e:
-        print(f"حدث خطأ غير متوقع أثناء تحميل 'ali_basic_creators.json': {e}")
-        ali_basic_creators = {'basic_creators': {}}
+        print("الملف 'ali_basic_creators.txt' غير موجود.")
     return ali_basic_creators
 
 def dump_ali_basic_creators(ali_basic_creators):
     try:
-        with open('backend/ali_basic_creators.json', 'w') as file:
-            json.dump(ali_basic_creators, file)
+        with open('backend/ali_basic_creators.txt', 'w') as file:
+            for creator_id in ali_basic_creators:
+                file.write(f"{creator_id}\n")
     except Exception as e:
-        print(f"حدث خطأ أثناء تفريغ 'ali_basic_creators.json': {e}")
+        print(f"حدث خطأ أثناء تفريغ 'ali_basic_creators.txt': {e}")
 
 def load_ali_owners():
+    ali_owners = {}
     try:
-        with open('backend/ali_owners.json', 'r') as file:
-            ali_owners = json.load(file)
+        with open('backend/ali_owners.txt', 'r') as file:
+            for line in file:
+                chat_id, owner_id = line.strip().split(':')
+                if chat_id not in ali_owners:
+                    ali_owners[chat_id] = {'owner_id': []}
+                ali_owners[chat_id]['owner_id'].append(owner_id)
     except FileNotFoundError:
-        print("الملف 'ali_owners.json' غير موجود.")
-        ali_owners = {'owners': {}}
-    except json.JSONDecodeError:
-        print("خطأ في قراءة بيانات JSON من 'ali_owners.json'.")
-        ali_owners = {'owners': {}}
-    except Exception as e:
-        print(f"حدث خطأ غير متوقع أثناء تحميل 'ali_owners.json': {e}")
-        ali_owners = {'owners': {}}
+        print("الملف 'ali_owners.txt' غير موجود.")
     return ali_owners
 
 def dump_ali_owners(ali_owners):
     try:
-        with open('backend/ali_owners.json', 'w') as file:
-            json.dump(ali_owners, file)
+        with open('backend/ali_owners.txt', 'w') as file:
+            for chat_id, data in ali_owners.items():
+                for owner_id in data['owner_id']:
+                    file.write(f"{chat_id}:{owner_id}\n")
     except Exception as e:
-        print(f"حدث خطأ أثناء تفريغ 'ali_owners.json': {e}")
+        print(f"حدث خطأ أثناء تفريغ 'ali_owners.txt': {e}")
 
 def load_ali_creators():
+    ali_creators = {}
     try:
-        with open('backend/ali_creators.json', 'r') as file:
-            ali_creators = json.load(file)
+        with open('backend/ali_creators.txt', 'r') as file:
+            for line in file:
+                chat_id, creator_id = line.strip().split(':')
+                if chat_id not in ali_creators:
+                    ali_creators[chat_id] = {'creator_id': []}
+                ali_creators[chat_id]['creator_id'].append(creator_id)
     except FileNotFoundError:
-        print("الملف 'ali_creators.json' غير موجود.")
-        ali_creators = {'creators': {}}
-    except json.JSONDecodeError:
-        print("خطأ في قراءة بيانات JSON من 'ali_creators.json'.")
-        ali_creators = {'creators': {}}
-    except Exception as e:
-        print(f"حدث خطأ غير متوقع أثناء تحميل 'ali_creators.json': {e}")
-        ali_creators = {'creators': {}}
+        print("الملف 'ali_creators.txt' غير موجود.")
     return ali_creators
 
 def dump_ali_creators(ali_creators):
     try:
-        with open('backend/ali_creators.json', 'w') as file:
-            json.dump(ali_creators, file)
+        with open('backend/ali_creators.txt', 'w') as file:
+            for chat_id, data in ali_creators.items():
+                for creator_id in data['creator_id']:
+                    file.write(f"{chat_id}:{creator_id}\n")
     except Exception as e:
-        print(f"حدث خطأ أثناء تفريغ 'ali_creators.json': {e}")
+        print(f"حدث خطأ أثناء تفريغ 'ali_creators.txt': {e}")
 
 def load_ali_admin():
+    ali_admin = {}
     try:
-        with open('backend/ali_admin.json', 'r') as file:
-            ali_admin = json.load(file)
+        with open('backend/ali_admin.txt', 'r') as file:
+            for line in file:
+                chat_id, admin_id = line.strip().split(':')
+                if chat_id not in ali_admin:
+                    ali_admin[chat_id] = {'admin_id': []}
+                ali_admin[chat_id]['admin_id'].append(admin_id)
     except FileNotFoundError:
-        print("الملف 'ali_admin.json' غير موجود.")
-        ali_admin = {'admin': {}}
-    except json.JSONDecodeError:
-        print("خطأ في قراءة بيانات JSON من 'ali_admin.json'.")
-        ali_admin = {'admin': {}}
-    except Exception as e:
-        print(f"حدث خطأ غير متوقع أثناء تحميل 'ali_admin.json': {e}")
-        ali_admin = {'admin': {}}
+        print("الملف 'ali_admin.txt' غير موجود.")
     return ali_admin
 
 def dump_ali_admin(ali_admin):
     try:
-        with open('backend/ali_admin.json', 'w') as file:
-            json.dump(ali_admin, file)
+        with open('backend/ali_admin.txt', 'w') as file:
+            for chat_id, data in ali_admin.items():
+                for admin_id in data['admin_id']:
+                    file.write(f"{chat_id}:{admin_id}\n")
     except Exception as e:
-        print(f"حدث خطأ أثناء تفريغ 'ali_admin.json': {e}")
+        print(f"حدث خطأ أثناء تفريغ 'ali_admin.txt': {e}")
 
 # دوال للتحقق من الرتب باستخدام المتغير a بدلاً من message
 def basic_dev(user_id):
     ali_basic_devs = load_ali_basic_devs()
-    return user_id in ali_basic_devs['basic_devs']
+    return str(user_id) in ali_basic_devs
 
 def dev(user_id):
     ali_devs = load_ali_devs()
-    return user_id in ali_devs['devs']
+    return str(user_id) in ali_devs
 
 def is_basic_creator(user_id):
     ali_basic_creators = load_ali_basic_creators()
-    return user_id in ali_basic_creators['basic_creators']
+    return str(user_id) in ali_basic_creators
 
 def owner(user_id, chat_id):
     ali_owners = load_ali_owners()
-    return chat_id in ali_owners['owners'] and user_id in ali_owners['owners'][chat_id]['owner_id']
+    return chat_id in ali_owners and str(user_id) in ali_owners[chat_id]['owner_id']
 
 def creator(user_id, chat_id):
     ali_creators = load_ali_creators()
-    return chat_id in ali_creators['creators'] and user_id in ali_creators['creators'][chat_id]['creator_id']
+    return chat_id in ali_creators and str(user_id) in ali_creators[chat_id]['creator_id']
 
 def admin(user_id, chat_id):
     ali_admin = load_ali_admin()
-    return chat_id in ali_admin['admin'] and user_id in ali_admin['admin'][chat_id]['admin_id']
+    return chat_id in ali_admin and str(user_id) in ali_admin[chat_id]['admin_id']
 
 # فحص ما إذا كان المستخدم مطورًا أو مالكًا باستخدام المتغير a
 def owner_id_ali(user_id):
