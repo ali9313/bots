97add6df703e84df68343ada18cde986cc47568e
ali9313 <lioali65808@gmail.com>
2024-09-29 01:32:58 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,14 +1,5 @@
-from config import *
-from rep import *
-from admin import *  # استيراد الملف الذي يحتوي على دوال مميزين
-from ali_json import *    # استيراد الملف الذي يحتوي على دوال المنشئين
-from malk import *
-from mmez import *
-from mnsha import *
-from mnshaas import *
-from mtor import *
-from tfael import *
-from thanoe import *
+from config import *  # إذا كانت تحتوي على إعدادات البوت مثل التوكن
+from rep import *  # إذا كانت تحتوي على الدالة reply_func
 
 # معالج الرسائل العامة
 @bot.message_handler(func=lambda a: True)
