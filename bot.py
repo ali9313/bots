a8bb961b4e4e310b7c7a70b54da77a79a004be6e
ali9313 <lioali65808@gmail.com>
2024-09-29 02:16:04 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,11 +1,14 @@
 from config import *  
 from rep import * 
 from ali_json import *
+import traceback  # لاستيراد مكتبة تتبع الأخطاء
+
 @bot.message_handler(func=lambda a: True)
 def echo_message(a):
     try:
         reply_func(a)  
     except Exception as e:
         print(f"حدث خطأ: {e}")
+        traceback.print_exc()  # لطباعة التفاصيل الكاملة للخطأ
 
 bot.polling(none_stop=True)
