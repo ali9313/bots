6ead2a3ca93963c4a3be95e212c2880b3a0826a4
ali9313 <lioali65808@gmail.com>
2024-09-29 13:30:35 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -4,14 +4,7 @@ from tfael import *
 from ali_json import *
 import traceback  
 
-@bot.message_handler(func=lambda a: True)
-def echo_message(a):
-    try:
-        reply_func(a)  
-    except Exception as e:
-        print(f"حدث خطأ: {e}")
-        traceback.print_exc()  
-
+# معالج للأوامر
 @bot.message_handler(commands=['check_dev', 'check_owner', 'check_basic_creator', 'check_admin', 'check_owner_id'])
 def handle_commands(a):
     try:
@@ -29,6 +22,15 @@ def handle_commands(a):
     except Exception as e:
         print(f"حدث خطأ أثناء معالجة الأمر: {e}")
         traceback.print_exc()
+
+# معالج للرسائل العامة
+@bot.message_handler(func=lambda a: True)
+def echo_message(a):
+    try:
+        reply_func(a)  
+    except Exception as e:
+        print(f"حدث خطأ: {e}")
+        traceback.print_exc()  
 
 # بدء البوت
 bot.polling(none_stop=True, timeout=60)
