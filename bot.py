e373029dd23e885bf660ab075c3fa07057a06d27
ali9313 <lioali65808@gmail.com>
2024-09-29 11:49:46 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -6,7 +6,11 @@ import traceback  # لاستيراد مكتبة تتبع الأخطاء
 @bot.message_handler(func=lambda a: True)
 def echo_message(a):
     try:
-        reply_func(a)  
+        # تحقق مما إذا كانت الرسالة تحتوي على الأمر 'تفعيل'
+        if a.text == "/تفعيل":
+            update_owners(a)  # استدعاء دالة update_owners مباشرة
+        else:
+            reply_func(a)  # استدعاء الدالة reply_func لبقية الرسائل
     except Exception as e:
         print(f"حدث خطأ: {e}")
         traceback.print_exc()  # لطباعة التفاصيل الكاملة للخطأ
