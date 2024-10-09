5c8355ce5211adf041ce16e1653fa174d27eb56d
ali9313 <lioali65808@gmail.com>
2024-10-09 14:09:56 +0300

    Add files via upload

--- ide.py
+++ ide.py
@@ -1,42 +1,62 @@
 from config import *
 from telebot.types import Message
 
-# دالة لجلب معلومات المستخدم
-def get_user_info(a: Message):
+# دالة لجلب معلومات الإنشاء
+def zzz_info(a: Message):
     user = a.reply_to_message.from_user if a.reply_to_message else a.from_user
     full_name = user.first_name + ' ' + user.last_name if user.last_name else user.first_name
     user_id = user.id
     username = f"@{user.username}" if user.username else "لا يـوجـد"
-    is_premium = "بـريميـوم" if user.is_premium else "عادي"
     
-    info_message = f"• معلومـات إنشـاء حسـاب تيليجـرام 📑:\n"
-    info_message += f"- الاسم: {full_name}\n"
-    info_message += f"- الايــدي: {user_id}\n"
-    info_message += f"- اليـوزر: {username}\n"
-    info_message += f"- الحساب: {is_premium}\n"
-    
-    return info_message
+    # هنا يجب استدعاء الدالة fetch_zelzal لجلب تاريخ الإنشاء (يمكن استبدالها ببيانات ثابتة للاختبار)
+    zelzal_sinc = "2023-01-01"  # استبدال هذه القيمة بالدالة الفعلية
 
-# دالة لجلب الصور
-def get_user_photos(a: Message):
+    ZThon = f'<a href="T.me/ZThon">ᯓ 𝗭𝗧𝗵𝗼𝗻 𝗧𝗲𝗹𝗲𝗴𝗿𝗮𝗺 𝗗𝗮𝘁𝗮 📟</a>'
+    ZThon += f"\n<b>⋆─┄─┄─┄─┄─┄─┄─⋆</b>\n\n"
+    ZThon += f"<b>• معلومـات إنشـاء حسـاب تيليجـرام 📑 :</b>\n"
+    ZThon += f"<b>- الاسـم    ⤎ </b> <a href='tg://user?id={user_id}'>{full_name}</a>"
+    ZThon += f"\n<b>- الايــدي   ⤎ </b> <code>{user_id}</code>"
+    ZThon += f"\n<b>- اليـوزر    ⤎  {username}</b>\n"
+    ZThon += f"<b>- الإنشـاء   ⤎</b>  {zelzal_sinc}  🗓"
+    return ZThon
+
+# دالة لجلب معلومات المستخدم
+def fetch_info(a: Message):
     user = a.reply_to_message.from_user if a.reply_to_message else a.from_user
-    photos = bot.get_user_profile_photos(user.id)
+    full_name = user.first_name + ' ' + user.last_name if user.last_name else user.first_name
+    user_id = user.id
+    username = f"@{user.username}" if user.username else "لا يـوجـد"
     
-    if photos.total_count > 0:
-        photo_file_id = photos.photos[0][-1].file_id  # جلب أحدث صورة بأعلى دقة
-        return photo_file_id
-    else:
-        return None
+    # مثال ثابت لتاريخ الإنشاء (يمكنك تعديل fetch_zelzal لاستخدام تاريخ حقيقي)
+    zelzal_sinc = "2023-01-01"
+    
+    # بيانات إضافية للمستخدم
+    user_bio = "لا يـوجـد" if not user.bio else user.bio
+    zzz = 500  # هذا العدد يجب أن يمثل عدد الرسائل (يمكنك استدعاء دالة لإحضار العدد الفعلي)
+    common_chat = 5  # عدد المجموعات المشتركة
+    
+    ZED_TEXT = "•⎚• مـعلومـات المسـتخـدم مـن بـوت زدثــون"
+    ZEDM = "✦ "
+    ZEDF = "⋆─┄─┄─┄─ ᶻᵗʰᵒᶰ ─┄─┄─┄─⋆"
+    
+    caption = f"<b>{ZED_TEXT} </b>\n"
+    caption += f"ٴ<b>{ZEDF}</b>\n"
+    caption += f"<b>{ZEDM}الاســم    ⤎ </b> <a href='tg://user?id={user_id}'>{full_name}</a>"
+    caption += f"\n<b>{ZEDM}اليـوزر    ⤎  {username}</b>"
+    caption += f"\n<b>{ZEDM}الايـدي    ⤎ </b> <code>{user_id}</code>\n"
+    caption += f"<b>{ZEDM}الرتبــه    ⤎ العضو 𓅫 </b>\n"
+    caption += f"<b>{ZEDM}الرسائل  ⤎</b>  {zzz} 💌\n"
+    caption += f"<b>{ZEDM}الـمجموعات المشتـركة ⤎  {common_chat}</b>\n"
+    caption += f"<b>{ZEDM}الإنشـاء  ⤎</b>  {zelzal_sinc}  🗓\n" 
+    caption += f"<b>{ZEDM}البايـو     ⤎  {user_bio}</b>\n"
+    return caption
 
-# أمر لعرض معلومات المستخدم
+def send_zzz_info(a: Message):
+    zzz_message = zzz_info(a)
+    bot.send_message(a.chat.id, zzz_message, parse_mode="HTML")
+
+
 def send_user_info(a: Message):
-    info_message = get_user_info(a)
-    bot.reply_to(a, info_message)
+    info_message = fetch_info(a)
+    bot.send_message(a.chat.id, info_message, parse_mode="HTML")
 
-# أمر لجلب صور المستخدم
-def send_user_photo(a: Message):
-    photo_file_id = get_user_photos(a)
-    if photo_file_id:
-        bot.send_photo(a.chat.id, photo_file_id)
-    else:
-        bot.reply_to(a, "لا يـوجـد صـور لهذا المستخدم.")
