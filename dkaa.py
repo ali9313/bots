582a98325add47e6a2f3904ccf28ed32febdd84e
ali9313 <lioali65808@gmail.com>
2024-10-12 16:29:14 +0300

    Add files via upload

---
+++ dkaa.py
@@ -0,0 +1,13 @@
+import requests
+from config import *
+def chat_with_gpt(a):
+    user_input = a.text[len("بوت "):]  # الحصول على السؤال بعد كلمة "بوت"
+    api_url = f"http://ttrtt.serv00.net/API/it_bero.php?promt={user_input}"
+    response = requests.get(api_url)
+
+    if response.status_code == 200:
+        gpt_response = response.text
+    else:
+        gpt_response = "عذرًا، لم أستطع الحصول على رد من API."
+
+    bot.reply_to(a, gpt_response)
